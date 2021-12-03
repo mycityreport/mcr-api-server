70d148139c44fc07d7e8506bd03f8d924f8e0758
Aruneko <aruneko99@gmail.com>
2021-12-03 11:10:44 +0900

    install ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,16 @@ repositories {
     mavenCentral()
 }
 
+val kotlinVersion = "1.6.0"
+val ktorVersion = "1.6.6"
+
 dependencies {
-    implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}")
+    implementation("io.ktor:ktor-server-core:${ktorVersion}")
+    implementation("io.ktor:ktor-server-netty:${ktorVersion}")
+    implementation("ch.qos.logback:logback-classic:1.2.7")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
