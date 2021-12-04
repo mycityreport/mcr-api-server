15b13129b145e032f793500c42f9aea6b0a8a60e
Aruneko <aruneko99@gmail.com>
2021-12-04 13:03:40 +0900

    add deps for json serializing

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}")
     implementation("io.ktor:ktor-server-core:${ktorVersion}")
     implementation("io.ktor:ktor-server-netty:${ktorVersion}")
+    implementation("io.ktor:ktor-serialization:${ktorVersion}")
     implementation("ch.qos.logback:logback-classic:1.2.7")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
