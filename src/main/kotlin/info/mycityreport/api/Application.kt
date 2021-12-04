f62fdf6b69b408cbb4902a930699892ea52a1b4e
Aruneko <aruneko99@gmail.com>
2021-12-04 17:35:39 +0900

    add route for health check endpoint

--- src/main/kotlin/info/mycityreport/api/Application.kt
+++ src/main/kotlin/info/mycityreport/api/Application.kt
@@ -1,12 +1,19 @@
 package info.mycityreport.api
 
+import info.mycityreport.api.healthcheck.presentation.registerHealthCheckRoutes
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        json()
+        registerHealthCheckRoutes()
+    }
     routing {
         get("/") {
             call.respondText("Hello World.")
