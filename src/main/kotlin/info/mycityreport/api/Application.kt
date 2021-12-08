c1a3a5427de33e4ec8382e452494129756bfb1b8
Aruneko <aruneko99@gmail.com>
2021-12-08 15:40:12 +0900

    enable cors for all hosts at once

--- src/main/kotlin/info/mycityreport/api/Application.kt
+++ src/main/kotlin/info/mycityreport/api/Application.kt
@@ -4,6 +4,7 @@ import info.mycityreport.api.healthcheck.presentation.registerHealthCheckRoutes
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.CORS
 import io.ktor.features.ContentNegotiation
 import io.ktor.response.respondText
 import io.ktor.routing.get
@@ -13,6 +14,9 @@ import io.ktor.serialization.json
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
+    install(CORS) {
+        anyHost()
+    }
     install(ContentNegotiation) {
         json()
         registerHealthCheckRoutes()
