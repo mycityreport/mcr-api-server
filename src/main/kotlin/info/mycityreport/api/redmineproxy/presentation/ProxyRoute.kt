6f4adb6334646712b800e0ca51878b2ef57d9306
Aruneko <aruneko99@gmail.com>
2021-12-10 11:44:22 +0900

    respond full url

--- src/main/kotlin/info/mycityreport/api/redmineproxy/presentation/ProxyRoute.kt
+++ src/main/kotlin/info/mycityreport/api/redmineproxy/presentation/ProxyRoute.kt
@@ -2,6 +2,7 @@ package info.mycityreport.api.redmineproxy.presentation
 
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.request.uri
 import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.get
@@ -11,7 +12,7 @@ import io.ktor.routing.routing
 fun Route.redmineProxyRouting() {
     route("/{...}") {
         get {
-            call.respondText(call.parameters.toString())
+            call.respondText(call.request.uri)
         }
     }
 }
