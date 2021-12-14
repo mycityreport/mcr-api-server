dbecead5e954f94dfd8d39352d424be985ad1b96
Aruneko <aruneko99@gmail.com>
2021-12-14 18:27:19 +0900

    use GetProxyController

--- src/main/kotlin/info/mycityreport/api/redmineproxy/presentation/GetProxyController.kt
+++ src/main/kotlin/info/mycityreport/api/redmineproxy/presentation/GetProxyController.kt
@@ -1,9 +1,30 @@
 package info.mycityreport.api.redmineproxy.presentation
 
+import info.mycityreport.api.redmineproxy.domain.entities.GETParameter
+import info.mycityreport.api.redmineproxy.domain.entities.HTTPHeader
+import info.mycityreport.api.redmineproxy.domain.entities.URLPath
+import info.mycityreport.api.redmineproxy.usecase.GetProxyResponse
 import info.mycityreport.api.redmineproxy.usecase.GetProxyUseCase
+import io.ktor.request.ApplicationRequest
+import io.ktor.request.path
+import io.ktor.util.toMap
 
-class GetProxyController(private val useCase: GetProxyUseCase) {
-    suspend fun callGetProxy(path: String) {
-//        val response = useCase.execute(path)
+class GetProxyController(private val useCase: GetProxyUseCase, private val request: ApplicationRequest) {
+    suspend fun callGetProxy(): GetProxyResponse {
+        val rawPath = pathParser(request.path())
+        val rawHeaders = request.headers.toMap()
+        val rawParams = request.queryParameters.toMap()
+        val path = URLPath(rawPath)
+        val headers = rawHeaders.map { HTTPHeader(it.key, it.value) }
+        val params = rawParams.map { GETParameter(it.key, it.value) }
+        return useCase.execute(path, headers, params)
+    }
+
+    private fun pathParser(rawPath: String): String {
+        // Ktor で request.path() を取得すると `/proxy/{...}` という状態で取れるので
+        // 前半部分の `/proxy` を切り取る処理を行う
+        val split = rawPath.split("/")
+        val actualPath = split.drop(2)
+        return "/$actualPath"
     }
 }
--- src/main/kotlin/info/mycityreport/api/redmineproxy/presentation/ProxyRoute.kt
+++ src/main/kotlin/info/mycityreport/api/redmineproxy/presentation/ProxyRoute.kt
@@ -1,21 +1,16 @@
 package info.mycityreport.api.redmineproxy.presentation
 
-import info.mycityreport.api.redmineproxy.domain.entities.GETParameter
-import info.mycityreport.api.redmineproxy.domain.entities.HTTPHeader
-import info.mycityreport.api.redmineproxy.domain.entities.URLPath
 import info.mycityreport.api.redmineproxy.usecase.GetProxyClient
 import info.mycityreport.api.redmineproxy.usecase.GetProxyUseCase
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
-import io.ktor.request.path
 import io.ktor.response.respondBytes
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.route
 import io.ktor.routing.routing
-import io.ktor.util.toMap
 import org.kodein.di.instance
 import org.kodein.di.ktor.closestDI
 
@@ -24,13 +19,8 @@ fun Route.redmineProxyRouting() {
         get {
             val getProxyClient by call.closestDI().instance<GetProxyClient>()
             val getProxyUseCase = GetProxyUseCase(getProxyClient)
-            val rawPath = call.request.path().split("/").filterNot { it == "proxy" }.joinToString("/")
-            val rawHeaders = call.request.headers.toMap()
-            val rawParams = call.request.queryParameters.toMap()
-            val path = URLPath(rawPath)
-            val headers = rawHeaders.map { HTTPHeader(it.key, it.value) }
-            val params = rawParams.map { GETParameter(it.key, it.value) }
-            val response = getProxyUseCase.execute(path, headers, params)
+            val controller = GetProxyController(getProxyUseCase, call.request)
+            val response = controller.callGetProxy()
             val contentType = ContentType.parse(response.contentType)
             val statusCode = HttpStatusCode.fromValue(response.statusCode)
             call.respondBytes(response.responseBody, contentType, statusCode)
