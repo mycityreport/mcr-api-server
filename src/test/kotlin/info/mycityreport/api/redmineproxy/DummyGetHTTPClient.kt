69ac00d827155c73985842e8061391ca0a103ba4
Aruneko <aruneko99@gmail.com>
2021-12-14 17:13:15 +0900

    use value class

--- src/test/kotlin/info/mycityreport/api/redmineproxy/DummyGetHTTPClient.kt
+++ src/test/kotlin/info/mycityreport/api/redmineproxy/DummyGetHTTPClient.kt
@@ -1,8 +1,11 @@
 package info.mycityreport.api.redmineproxy
 
+import info.mycityreport.api.redmineproxy.domain.entities.ContentType
 import info.mycityreport.api.redmineproxy.domain.entities.GETParameter
 import info.mycityreport.api.redmineproxy.domain.entities.GetResponse
 import info.mycityreport.api.redmineproxy.domain.entities.HTTPHeader
+import info.mycityreport.api.redmineproxy.domain.entities.HTTPStatusCode
+import info.mycityreport.api.redmineproxy.domain.entities.ResponseBody
 import info.mycityreport.api.redmineproxy.domain.entities.URLPath
 import info.mycityreport.api.redmineproxy.usecase.GetProxyClient
 
@@ -12,9 +15,9 @@ class DummyGetHTTPClient : GetProxyClient {
         httpHeaders: List<HTTPHeader>?,
         getParams: List<GETParameter>?
     ): GetResponse {
-        val statusCode = 200
-        val contentType = "text/plain"
-        val body = path.value.toByteArray()
+        val statusCode = HTTPStatusCode(200)
+        val contentType = ContentType("text/plain")
+        val body = ResponseBody(path.value.toByteArray())
         return GetResponse(statusCode, contentType, body)
     }
 }
