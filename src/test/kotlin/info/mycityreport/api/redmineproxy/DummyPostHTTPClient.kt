2f7d9da3b0e752c14255e723ee98460cd124b296
Aruneko <aruneko99@gmail.com>
2021-12-15 17:48:58 +0900

    receive response body

--- src/test/kotlin/info/mycityreport/api/redmineproxy/DummyPostHTTPClient.kt
+++ src/test/kotlin/info/mycityreport/api/redmineproxy/DummyPostHTTPClient.kt
@@ -9,11 +9,11 @@ import info.mycityreport.api.redmineproxy.domain.entities.ResponseBody
 import info.mycityreport.api.redmineproxy.domain.entities.URLPath
 import info.mycityreport.api.redmineproxy.usecase.PostProxyClient
 
-class DummyPostHTTPClient(private val status: Int) : PostProxyClient {
+class DummyPostHTTPClient(private val status: Int, private val response: String) : PostProxyClient {
     override suspend fun post(path: URLPath, httpHeaders: List<HTTPHeader>, requestBody: RequestBody): HTTPResponse {
         val statusCode = HTTPStatusCode(status)
         val contentType = ContentType("application/json")
-        val responseBody = ResponseBody(requestBody.value)
+        val responseBody = ResponseBody(response.toByteArray())
 
         return HTTPResponse(statusCode, contentType, responseBody)
     }
