cf14fb66e16981f68224cd846cda57c860a61869
Aruneko <aruneko99@gmail.com>
2021-12-15 17:49:10 +0900

    add failing pattern

--- src/test/kotlin/info/mycityreport/api/redmineproxy/PostRoutingTest.kt
+++ src/test/kotlin/info/mycityreport/api/redmineproxy/PostRoutingTest.kt
@@ -17,20 +17,42 @@ internal class PostRoutingTest {
     @Test
     fun `プロキシ対象のアドレスにPOSTできる`() {
         // given
+        val responseMessage = "created"
         val dependencies = DI {
-            bind<PostProxyClient>() { singleton { DummyPostHTTPClient(201) } }
+            bind<PostProxyClient>() { singleton { DummyPostHTTPClient(201, responseMessage) } }
         }
         val path = "/proxy/issues.json"
         val body = "{\"name\": \"sample\"}"
 
         // when
-
-        // then
         withTestApplication({ module(dependencies) }) {
             handleRequest(HttpMethod.Post, path) {
                 setBody(body)
             }.apply {
+                // then
                 assertEquals(HttpStatusCode.Created, response.status())
+                assertEquals(responseMessage, response.content)
+            }
+        }
+    }
+
+    @Test
+    fun `プロキシ先で起きたPOSTのエラーがそのまま返ってくる`() {
+        val responseMessage = "Invalid Format"
+        val dependencies = DI {
+            bind<PostProxyClient>() { singleton { DummyPostHTTPClient(400, responseMessage) } }
+        }
+        val path = "/proxy/issues.json"
+        val body = "{\"id\": \"3\"}"
+
+        // when
+        withTestApplication({ module(dependencies) }) {
+            handleRequest(HttpMethod.Post, path) {
+                setBody(body)
+            }.apply {
+                // then
+                assertEquals(HttpStatusCode.BadRequest, response.status())
+                assertEquals(responseMessage, response.content)
             }
         }
     }
